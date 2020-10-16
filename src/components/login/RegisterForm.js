a9ef6858909bebfdd3b0d7778aa2f7f11cd78cd2
Daniel Fishman <fishfry62@gmail.com>
2020-10-16 09:50:02 -0700

    fixed misnamed classes

--- src/components/login/RegisterForm.js
+++ src/components/login/RegisterForm.js
@@ -20,21 +20,21 @@ export default function RegisterForm(props) {
 
 
     return (
-        <div className="Form">
-            <div className="FormInput">
+        <div className="form">
+            <div className="formInput">
             <label>Full Name</label>
             <input type='text' placeholder='John Doe' onChange={(e)=>setName(e.target.value)}></input>
             </div>
-            <div className="FormInput">
+            <div className="formInput">
             <label>Email</label>
             <input type='email' placeholder='user@email.com' onChange={(e)=>setEmail(e.target.value)}></input>
             </div>
-            <div className="FormInput">
+            <div className="formInput">
             <label>Password</label>
             <input type='password' placeholder='****************' onChange={(e)=>setPwd(e.target.value)}></input>
             </div>
 
-            <button className="SubmitBtn" onClick={onFormSubmit}>Sign-Up</button>
+            <button className="submitBtn" onClick={onFormSubmit}>Sign-Up</button>
         </div>
     );
 }
