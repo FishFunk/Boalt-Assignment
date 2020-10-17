938ac9798b3a7df6c86fa2607b40105aed998544
Daniel Fishman <fishfry62@gmail.com>
2020-10-17 11:37:07 -0700

    nav styling and entry animation polish

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,12 +9,32 @@ import {
 export default function HomePage() {
     // let history = useHistory();
 
+    // TODO: Make nav responsive - check out react-media package
+
     return (
     <div className='container'>
-        <header className={`animated ${styles.homeHeader} ${styles.shrinkLeft}`}>
-            <FontAwesomeIcon icon={faApple} className={styles.headerLogo} size='2x' />
+        <header className={styles.homeHeader}>
+            <div className={`animated ${styles.headerLogoWrapper} ${styles.shrinkLeft}`}>
+                <FontAwesomeIcon icon={faApple} className={styles.headerLogo} size='2x' />
+            </div>
+            <div className={`animated ${styles.navWrapper} ${styles.popInLeft}`}>
+                <ul className={styles.nav}>
+                    <li className={styles.navItem}>
+                        <a>iPhone</a>
+                    </li>
+                    <li className={styles.navItem}>
+                        <a>Macbook Pro</a>
+                    </li>
+                    <li className={styles.navItem}>
+                        <a>Watch</a>
+                    </li>
+                    <li className={styles.navItem}>
+                        <button className={styles.notifyBtn}>Notify Me</button>
+                    </li>
+                </ul>
+            </div>
         </header>   
-        <div className='content backgroundGradient'>
+        <div className={`${styles.content}`}>
 
         </div>
     </div>);
